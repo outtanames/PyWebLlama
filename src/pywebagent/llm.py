3a794e25a24dc3027dd662c3f9f4f24ab6a4b333
Nikil Ravi <nravi@stanford.edu>
2024-08-03 15:56:18 -0700

    Add 70b, remove example file

--- src/pywebagent/llm.py
+++ src/pywebagent/llm.py
@@ -19,3 +19,22 @@ def llama_405b(prompt: str, provider="baseten"):
      raise NotImplementedError("Groq is not yet supported")
   else:
      raise ValueError("Invalid provider")
+  
+def llama_70b(prompt: str, provider="baseten"):
+  if provider == "baseten":
+    s = requests.Session()
+    with s.post(
+        "https://model-jwd78r4w.api.baseten.co/production/predict",
+        headers={"Authorization": "Api-Key vB3H7OSq.HCfqgTyxgyYKAp5palSauFwCV5UzAxSp"},
+        data=json.dumps({
+          "prompt": prompt,
+          "stream": True,
+          "max_tokens": 1024
+        }),
+        stream=False,
+    ) as resp:
+        resp.content
+  elif provider == "groq":
+     raise NotImplementedError("Groq is not yet supported")
+  else:
+     raise ValueError("Invalid provider")
