8289276afd6a6dc4e8f3bcb2b4e060ff182d7876
Nikil Ravi <nravi@stanford.edu>
2024-08-03 15:54:56 -0700

    Add Llama as a function call

---
+++ src/pywebagent/llm.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def llama_405b(prompt: str, provider="baseten"):
+  if provider == "baseten":
+    s = requests.Session()
+    with s.post(
+        "https://model-7wlxp82w.api.baseten.co/production/predict",
+        headers={"Authorization": "Api-Key QLk2AhIS.ay5p1g5DPDeAcNqveNziEOmh2hW42b6Q"},
+        data=json.dumps({
+          "prompt": prompt,
+          "stream": True,
+          "max_tokens": 1024
+        }),
+        stream=False,
+    ) as response:
+        return str(response.content)
+  elif provider == "groq":
+     raise NotImplementedError("Groq is not yet supported")
+  else:
+     raise ValueError("Invalid provider")
